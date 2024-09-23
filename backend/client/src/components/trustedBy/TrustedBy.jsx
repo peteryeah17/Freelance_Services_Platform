c3d03c67d7b4731531629a71906b1b75af5c480c
peteryeah17 <peteryeah17@gmail.com>
2024-09-23 21:45:08 +0800

    home page color updated

--- backend/client/src/components/trustedBy/TrustedBy.jsx
+++ backend/client/src/components/trustedBy/TrustedBy.jsx
@@ -5,7 +5,7 @@ const TrustedBy = () => {
   return (
     <div className="trustedBy">
       <div className="container">
-        <span></span>
+        <span></span> 
         <img src="https://static.www.nfl.com/image/upload/v1554321393/league/nvfr7ogywskqrfaiu38m.svg" alt="" />
         <img src="https://upload.wikimedia.org/wikipedia/en/thumb/0/03/National_Basketball_Association_logo.svg/210px-National_Basketball_Association_logo.svg.png" alt="" />
         <img src="https://upload.wikimedia.org/wikipedia/en/thumb/3/3a/05_NHL_Shield.svg/1200px-05_NHL_Shield.svg.png" alt="" />
