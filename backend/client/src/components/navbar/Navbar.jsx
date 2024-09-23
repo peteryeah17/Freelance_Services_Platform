80abff8d678defa36021e642d43b74eb1dbc08c7
peteryeah17 <peteryeah17@gmail.com>
2024-09-23 17:36:24 +0800

    video upload

--- backend/client/src/components/navbar/Navbar.jsx
+++ backend/client/src/components/navbar/Navbar.jsx
@@ -39,12 +39,11 @@ function Navbar() {
       <div className="container">
         <div className="logo">
           <Link className="link" to="/">
-            <span className="text">fiverr</span>
+            <span className="text">GearUp</span>
           </Link>
-          <span className="dot">.</span>
         </div>
         <div className="links">
-          <span>Fiverr Business</span>
+          <span>GearUp Business</span>
           <span>Explore</span>
           <span>English</span>
           {!currentUser?.isSeller && <span>Become a Seller</span>}
